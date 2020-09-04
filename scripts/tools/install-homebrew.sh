dabe889ba760ffc7a1d7f444930d4a2f83e3bca3
Mattias Alm <mattiasalm@gmail.com>
2020-09-04 09:08:56 +0200

    update homebrew install script

--- scripts/tools/install-homebrew.sh
+++ scripts/tools/install-homebrew.sh
@@ -7,8 +7,8 @@ printInstalling "Homebrew"
 if hash brew 2>/dev/null; then
     printNoteSuccess "Homebrew already installed"
 else
-    local url=https://raw.githubusercontent.com/Homebrew/install/master/install
-    if /usr/bin/ruby -e "$(curl -fsSL ${url})"; then
+    local url=https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+    if /bin/bash -c "$(curl -fsSL ${url})"; then
         printNoteSuccess "Installation succeeded"
     else
         printNoteError "Installation failed"
